74db2996599aaddec2a6176ec2d8cd6b9f2e5521
ZeroZipZilch <0zipzilch@gmail.com>
2020-12-08 11:15:39 +0100

    Only start server is DB connect succeeds. Remove all semi-colons in server.ts

--- meta-server/server.ts
+++ meta-server/server.ts
@@ -1,28 +1,30 @@
-import cors from 'cors';
-import express from 'express';
-import morgan from 'morgan';
+import cors from 'cors'
+import express from 'express'
+import morgan from 'morgan'
 
 import { connect } from 'mongoose'
 
 import projectRoutes from './routes/projectRoutes'
-import paramsHack from './middleware/paramsHack';
+import paramsHack from './middleware/paramsHack'
 
 // Configs and middleware
 connect(process.env.DB_URI || 'mongodb://localhost:27017/meta-api', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+}).then(() => {
+  const app = express()
+  app.use(morgan('dev'))
+  app.use(express.json())
+  app.use(cors())
+
+  const PORT = process.env.PORT || 8080
+
+  // Routes
+  app.use('/:user_name/projects', paramsHack, projectRoutes)
+  app.use('/projects', projectRoutes)
+
+  // Connect to DB and start the server
+  app.listen(PORT, () => console.log(`⚡️[meta-server] Listening on port ${PORT}`))
+}).catch(e => {
+  console.error("Couldn't start server. A database error occured while connecting!")
 })
-
-const app = express();
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(cors());
-
-const PORT = process.env.PORT || 8080
-
-// Routes
-app.use('/:user_name/projects', paramsHack, projectRoutes)
-app.use('/projects', projectRoutes)
-
-// Connect to DB and start the server
-app.listen(PORT, () => console.log(`⚡️[meta-server] Listening on port ${PORT}`));
