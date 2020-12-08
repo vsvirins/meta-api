3afac49c88a57aafef4527d7af4a89bfe39fbb9f
ZeroZipZilch <0zipzilch@gmail.com>
2020-12-08 10:13:23 +0100

    + API_VERSION const. Changed to individual consts

--- meta-server/config/Constants.ts
+++ meta-server/config/Constants.ts
@@ -1,5 +1,2 @@
-class Constants {
-  static DB_CONNECTION_STRING: string  = "mongodb://localhost/meta-api"
-}
-
-export = Constants
+export const DB_CONNECTION_STRING: string = "mongodb://localhost/meta-api"
+export const API_VERSION: string = "0.1"
