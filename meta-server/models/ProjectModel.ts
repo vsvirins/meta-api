14f57f75368a60847f7d4d0afcd49bc9ae903348
ZeroZipZilch <0zipzilch@gmail.com>
2020-12-08 11:35:24 +0100

    Cleaned up model files

--- meta-server/models/ProjectModel.ts
+++ meta-server/models/ProjectModel.ts
@@ -1,16 +1,10 @@
 import { Schema, connection } from 'mongoose'
 
-class ProjectSchema {
-  static get schema () {
-    const schema = new Schema({
-      user_name: { type: String, required: true },
-      project_name: { type: String, required: true },
-      enpoints: { type: Schema.Types.ObjectId, ref: "Endpoints", required: false }
-    })
+const schema = new Schema({
+  user_name: { type: String, required: true },
+  project_name: { type: String, required: true },
+  enpoints: { type: Schema.Types.ObjectId, ref: "Endpoints", required: false }
+})
 
-    return schema;
-  }
-}
-
-const model = connection.model("Projects", ProjectSchema.schema)
+const model = connection.model("Projects", schema)
 export = model
